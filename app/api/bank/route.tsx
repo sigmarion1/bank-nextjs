ba3e651a2fd1da6a1ced9ed756b411d7c56f5c78
sigmarion1 <axsim@naver.com>
2024-02-10 11:58:04 +0900

    apply dynamic

--- app/api/bank/route.tsx
+++ app/api/bank/route.tsx
@@ -2,6 +2,8 @@
 import { db, sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
 
     // const client = await db.connect()
