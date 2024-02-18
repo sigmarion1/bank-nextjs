4c1ee68eaa623691ad484576aed81238c98d5151
sigmarion1 <axsim@naver.com>
2024-02-19 01:05:53 +0900

    change interest

--- app/api/transactions/route.tsx
+++ app/api/transactions/route.tsx
@@ -7,7 +7,7 @@ export const dynamic = 'force-dynamic';
 export async function POST() {
 
     try {
-        await sql`update bank.accounts set balance = balance * 1.0002;`;
+        await sql`update bank.accounts set balance = balance * 1.0003;`;
     } catch (err) {
         return NextResponse.json({ err })
     }
