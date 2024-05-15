40f74016ca6e073e5c8300486dfabfbd4463ea5d
sigmarion1 <axsim@naver.com>
2024-05-15 15:21:22 +0900

    change db vercel -> postgres

--- app/api/accounts/route.tsx
+++ app/api/accounts/route.tsx
@@ -1,6 +1,7 @@
 // import { NextApiRequest, NextApiResponse } from "next";
-import { db, sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
+import pool from '../../lib/db';
+
 
 export const dynamic = 'force-dynamic';
 
@@ -9,11 +10,10 @@ export async function GET() {
     let account;
 
     try {
-        account = await sql`SELECT * from bank.accounts where account_id=1;`;
+        account = await pool.query('SELECT * from bank.accounts where account_id=1;');
     } catch (err) {
         return NextResponse.json({ err })
     }
-    // console.log(account)
 
     return NextResponse.json({ data: account.rows[0] })
 
---
+++ app/lib/db.ts
@@ -0,0 +1,12 @@
+// lib/db.ts
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+export default pool;
